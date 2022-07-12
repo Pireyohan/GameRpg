9870306c45fb00463603dd58929e37eeee245b48
Pire Yohan <yopiro@hotmail.fr>
2022-07-12 16:07:20 +0200

    "Model abstract"

---
+++ utils/Model.java
@@ -0,0 +1,13 @@
+package vscode_rpg_correction.utils;
+
+public abstract class Model {
+    public int id=0;
+    
+    public abstract boolean get(int id);
+    public abstract boolean save();
+
+    
+
+
+    
+}
